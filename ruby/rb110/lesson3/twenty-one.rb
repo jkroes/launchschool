44af4d7cc11ca368f97abbdebe94946de61836f3
Justin Kroes <jkroes14@ucsbalum.com>
2024-03-03 21:59:31 -0800

    Twenty-one Bonus Feature 4

--- ruby/rb110/lesson3/twenty-one.rb
+++ ruby/rb110/lesson3/twenty-one.rb
@@ -67,14 +67,33 @@ def dealer_stay?(total)
   total >= 17
 end
 
-def winner(human_total, computer_total)
+def determine_result!(human_total, computer_total, score)
   if bust? human_total
-    "You busted. The dealer wins!"
+    score[:dealer] += 1
+    :player_busted
   elsif bust? computer_total
-    "The dealer busted. You win!"
+    score[:player] += 1
+    :dealer_busted
   elsif human_total > computer_total
-    'You won!'
+    score[:player] += 1
+    :player_won
   elsif human_total < computer_total
+    score[:dealer] += 1
+    :dealer_won
+  else
+    :tie
+  end
+end
+
+def result_msg(result)
+  case result
+  when :player_busted
+    "You busted. The dealer wins!"
+  when :dealer_busted
+    "The dealer busted. You win!"
+  when :player_won
+    'You won!'
+  when :dealer_won
     'The dealer won!'
   else
     "It's a tie!"
@@ -130,89 +149,108 @@ def play_again?
   gets.chomp.downcase.start_with? "y"
 end
 
-def display_results(human, computer, human_total, computer_total)
+def display_results(human, computer, human_total, computer_total, score)
   prompt "Comparing hands to determine the outcome of this game."
   prompt_player(human, human_total)
   prompt_dealer(computer, computer_total)
   puts ""
   puts "=" * 40
-  prompt winner(human_total, computer_total)
+  result = determine_result!(human_total, computer_total, score)
+  prompt result_msg(result)
   puts "=" * 40
   puts ""
 end
 
 loop do
   system 'clear'
-  prompt "Welcome to blackjack."
+  prompt "Welcome to blackjack"
+  prompt "Let's play a match"
+  prompt "First to five wins the match"
   puts ""
 
-  player = []
-  dealer = []
-  player_total = 0
-  dealer_total = 0
-  deck = initialize_deck
-
-  2.times do
-    deal!(deck, player)
-    deal!(deck, dealer)
-  end
-
-  player_total = total(player)
-  dealer_total = total(dealer)
-
-  prompt "Player turn starts"
-  prompt "The dealer holds #{join_hand(dealer[1])} and an unknown card"
-  prompt_player_cards(player)
+  wins = {player: 0, dealer: 0}
 
   loop do
-    prompt_player_total(player_total)
-    break if blackjack?(player_total)
-    break if bust? player_total
+    player = []
+    dealer = []
+    player_total = 0
+    dealer_total = 0
+    deck = initialize_deck
 
-    prompt "Do you want to h(it) or s(tay)?"
-
-    hit_or_stay = gets.chomp.downcase
-    if hit_or_stay.start_with? "h"
-      prompt "Dealt %s" % deal!(deck, player)
-      player_total = total(player)
-    elsif hit_or_stay.start_with? "s"
-      break
-    else
-      "Please select one of the two valid options"
+    2.times do
+      deal!(deck, player)
+      deal!(deck, dealer)
     end
-  end
 
-  prompt "Player turn has concluded"
-  puts ""
-
-  if bust? player_total
-    display_results(player, dealer, player_total, dealer_total)
-    play_again? ? next : break
-  end
-
-  prompt "Dealer turn starts"
-  prompt_dealer_cards(dealer)
-
-  loop do
-    prompt_dealer_total(dealer_total)
-    break if dealer_stay?(dealer_total)
-    prompt "Dealt %s" % deal!(deck, dealer)
+    player_total = total(player)
     dealer_total = total(dealer)
+
+    prompt "The current score is Player: #{wins[:player]}, Dealer: #{wins[:dealer]}"
+    puts ""
+    prompt "Player turn starts"
+    prompt "The dealer holds #{join_hand(dealer[1])} and an unknown card"
+    prompt_player_cards(player)
+
+    loop do
+      prompt_player_total(player_total)
+      break if blackjack?(player_total)
+      break if bust? player_total
+
+      prompt "Do you want to h(it) or s(tay)?"
+
+      hit_or_stay = gets.chomp.downcase
+      if hit_or_stay.start_with? "h"
+        prompt "Dealt %s" % deal!(deck, player)
+        player_total = total(player)
+      elsif hit_or_stay.start_with? "s"
+        break
+      else
+        "Please select one of the two valid options"
+      end
+    end
+
+    prompt "Player turn has concluded"
+    puts ""
+
+    if bust? player_total
+      display_results(player, dealer, player_total, dealer_total, wins)
+      wins.values.any? { |score| score == 5 } ? break : next
+    end
+
+    prompt "Dealer turn starts"
+    prompt_dealer_cards(dealer)
+
+    loop do
+      prompt_dealer_total(dealer_total)
+      break if dealer_stay?(dealer_total)
+      prompt "Dealt %s" % deal!(deck, dealer)
+      dealer_total = total(dealer)
+    end
+
+    prompt "Dealer turn has concluded"
+    puts ""
+
+    if bust? dealer_total
+      display_results(player, dealer, player_total, dealer_total, wins)
+      wins.values.any? { |score| score == 5 } ? break : next
+    end
+
+    display_results(player, dealer, player_total, dealer_total, wins)
+    break if wins.values.any? { |score| score == 5 }
   end
 
-  prompt "Dealer turn has concluded"
-  puts ""
-
-  if bust? dealer_total
-    display_results(player, dealer, player_total, dealer_total)
-    play_again? ? next : break
+  if wins[:player] == 5
+    prompt "You won the match!"
+  else
+    prompt "The dealer won the match!"
   end
 
-  display_results(player, dealer, player_total, dealer_total)
-  break unless play_again?
+  play_again? ? next : break
 end
 
 prompt "Thank you for playing!"
+
+
 
 # Bonus Feature 2: The last call to play_again? differs from
 # earlier ones in that it doesn't use the ternary option to
